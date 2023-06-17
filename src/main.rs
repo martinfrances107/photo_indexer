1afbcd16362db3b38b60151494dd82dc5cb12272
Martin <martinfrances107@hotmail.com>
2023-06-17 10:40:02 +0100

    Better loggin as the server turns on.

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,14 @@ async fn main() -> std::io::Result<()> {
     use leptos::view;
     use leptos_actix::generate_route_list;
     use leptos_actix::LeptosRoutes;
+    use log::log;
 
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(|cx| view! { cx, <App/> });
 
-    HttpServer::new(move || {
+    match HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
 
@@ -30,9 +31,18 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", site_root))
         //.wrap(middleware::Compress::default())
     })
-    .bind(&addr)?
-    .run()
-    .await
+    .bind(&addr)
+    {
+        Ok(server) => {
+            log::info!("hello");
+            println!("Server started on  {addr}");
+            server.run().await
+        }
+        Err(e) => {
+            log::error!("Could not start server");
+            Err(e)
+        }
+    }
 }
 
 #[cfg(not(any(feature = "ssr", feature = "csr")))]
