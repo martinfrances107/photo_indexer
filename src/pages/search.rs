226168232fb78335a61a9dbe46ebc637fd2b44d0
martinfrances107 <martinfrances107@hotmail.com>
2023-09-19 12:49:24 +0100

    Introduced side_tray containing meta data.

--- src/pages/search.rs
+++ src/pages/search.rs
@@ -14,6 +14,7 @@ use crate::indexer::Index;
 pub fn Search() -> impl IntoView {
     let root = Path::new(&"../exif-samples");
 
+    let (open_tray_get, open_tray_set) = create_signal::<Option<usize>>(None);
     let (index_get, _index_set) = create_signal(Index::new(root));
     let (search_query_get, search_query_set) = create_signal::<Vec<char>>(vec![]);
 
@@ -69,62 +70,94 @@ pub fn Search() -> impl IntoView {
 
          <p class="mb-2">{ move || summary()}</p>
 
-         <section class="flex flex-wrap gap-y-4 rounded px-2 py-4 justify-evenly dark:text-slate-950 bg-slate-600" >
-           <Transition
-             fallback =move || view!{ <p>"Loading"</p> }
-           >
+         <Transition
+           fallback =move || view!{ <p>"Loading"</p> }
+         >
+        <section class="
+          flex
+          flex-wrap
+          gap-y-4
+          rounded
+          px-2 py-4
+          justify-evenly
+          dark:text-slate-950 bg-slate-600" >
 
-            <For
-              each=move || images()
-              key=move |(i, _)| *i
-              view=move |(_, (pb, _))| {
-                 view!{
-                    <div class="p-2 mb-4 rounded text-left" style="width:430px;">
-                      <figure class="bg-slate-100 rounded-t">
-                         <img
-                           width="420" height="420"
-                           class="aspect-square mx-auto"
-                           src={pb.clone().into_os_string().into_string().unwrap()}
-                         />
-                         <figcaption>
-                           {pb.file_name().unwrap().to_str().unwrap().to_string()}
-                           <p>
-
-                               {
-                                let ds = index_get.get().description_store.clone();
-                                match ds.get(&pb) {
-                                  Some(name) => view!{<p class="break-words w-full">{name}</p>},
-                                  None => view!{<p class="w-full">"No description"</p>}
-                                }
-                              }
-
-                           </p>
-                         </figcaption>
-                      </figure>
-                      <details class="bg-slate-100 rounded-b">
-                        <summary>
-                          MetaData
-                        </summary>
-                        <div class="[&>*:nth-child(even)]:bg-gray-100 [&>*:nth-child(odd)]:bg-gray-300">
-                          <For
-                          // each = move || { 1i32..10i32}
-                          each =move || { md_store().get(&pb).expect("failed to extract fields from md_store").clone()}
-                          // each = move || { index_get.get().md_store}
-                          key = move |field| {field.ifd_num}
-                          view = move |field| { view!{
-                            <p>{ field.tag.to_string() }</p>
-                            <p class="text-right" >{ field.display_value().to_string() }</p>
-                          }}
-                          />
-                        </div>
-                      </details>
-                     </div>
-                  }
+        <For
+          each=move || images()
+          key=move |(i, _)| format!("{}", *i)
+          view=move |(i, (pb, _))| {
+            view!{
+              <div
+                id = move || format!("md-{i}")
+                class="
+                block-inline
+                [&>*:nth-child(even)]:bg-gray-100
+                [&>*:nth-child(odd)]:bg-gray-300
+                overflow-hidden
+                w-0
+                }}">
+              <For
+                each =move || { md_store().get(&pb).expect("failed to extract fields from md_store").clone()}
+                key = move |field| {field.ifd_num}
+                view = move |field| {
+                  view!{
+                  <p>{ field.tag.to_string() }</p>
+                  <p class="text-right" >{ field.display_value().to_string() }</p>
+                }
               }
-            />
+              />
+              </div>
+            }}
+        />
 
-          </Transition>
-        </section>
-       </div>
+        <For
+          each=move || images()
+          key=move |(i, _)| *i
+          view=move |(_, (pb, _))| {
+             view!{
+                <div class="p-2 mb-4 rounded text-left" style="width:280px;">
+                  <figure class="bg-slate-100 rounded-t">
+                     <img
+                       width="274" height="160"
+                       class="aspect-square mx-auto"
+                       src={pb.clone().into_os_string().into_string().unwrap()}
+                     />
+                     <figcaption>
+                       {pb.file_name().unwrap().to_str().unwrap().to_string()}
+                       <p>
+                        {
+                            let ds = index_get.get().description_store.clone();
+                            match ds.get(&pb) {
+                              Some(name) => view!{<p class="break-words w-full">{name}</p>},
+                              None => view!{<p class="w-full">"No description"</p>}
+                            }
+                          }
+                    </p>
+                     </figcaption>
+                  </figure>
+                  // <A on::clip = move || {}>Meta Data</A>
+                  // <details class="bg-slate-100 rounded-b">
+                  //   <summary>
+                  //     MetaData
+                  //   </summary>
+                  //   <div class="[&>*:nth-child(even)]:bg-gray-100 [&>*:nth-child(odd)]:bg-gray-300">
+                  //     <For
+                  //     each =move || { md_store().get(&pb).expect("failed to extract fields from md_store").clone()}
+                  //     key = move |field| {field.ifd_num}
+                  //     view = move |field| { view!{
+                  //       <p>{ field.tag.to_string() }</p>
+                  //       <p class="text-right" >{ field.display_value().to_string() }</p>
+                  //     }}
+                  //     />
+                  //   </div>
+                  // </details>
+                 </div>
+              }
+          }
+        />
+
+       </section>
+      </Transition>
+    </div>
     }
 }
