8c61217e9d47c0b1626cad67c1876d4ddbe4a1c4
martinfrances107 <martinfrances107@hotmail.com>
2024-06-03 20:22:00 +0100

    Removed unused tailwind classes.

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -9,7 +9,7 @@ use crate::settings::SettingsPannel;
 pub fn About() -> impl IntoView {
     view! {
       <div class="flex">
-        <div class="dark::bg-slate-600 my-0 mx-auto">
+        <div class="my-0 mx-auto">
           <p>"Search a directory for images query exif-meta data"</p>
           <p>"Subdirectories are included."</p>
         </div>
